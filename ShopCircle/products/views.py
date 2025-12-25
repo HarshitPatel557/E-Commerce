1a1a3aa2b44fc6a4f6fb1ae0541d84a787045df4
HarshitPatel557 <patelharshithp404@gmail.com>
2025-12-25 10:21:15 +0530

    adding modelserializers and urls

--- ShopCircle/products/views.py
+++ ShopCircle/products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view,APIView
+from .models import Product,Category
+from .serializers import CategorySerializer,ProductSerializer
 # Create your views here.
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories,many=True)
+    return Response(serializer.data)
+
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products,many=True)
+    return Response(serializer.data)
+
+def product_detail(request,pk):
+    product = Product.objects.get(pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
