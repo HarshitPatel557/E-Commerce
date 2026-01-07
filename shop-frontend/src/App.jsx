416ee8d969ec1c4f83f63b5b300b3160b7bb45c7
HarshitPatel557 <patelharshithp404@gmail.com>
2026-01-07 17:59:25 +0530

    Improving the app.jsx and adding Comments

--- shop-frontend/src/App.jsx
+++ shop-frontend/src/App.jsx
@@ -1,26 +1,35 @@
+// CSS
 import './App.css'
+import { OrbitProgress } from "react-loading-indicators";
+
+// React
+import { Routes,Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+// utils
+import { apiFetch } from "./utils/api";
+import { getAccessToken } from "./utils/auth";
+
+// Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import PrivateRoute from "./components/PrivateRoute";
+
+// Pages
 import ProductList from './pages/ProductList'
 import ProductDetail from './pages/ProductDetail'
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
 import OrderSuccess from './pages/OrderSuccess';
-import { Routes,Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { useEffect, useState } from 'react';
-import { OrbitProgress } from "react-loading-indicators";
 import Login from "./pages/Login";
-import PrivateRoute from "./components/PrivateRoute";
-import { apiFetch } from "./utils/api";
-import { getAccessToken } from "./utils/auth";
-import Footer from './components/Footer';
 import Register from './pages/Register';
 import Profile from "./pages/Profile";
 
 
-
+// App component
 function App() {
   
-  const [cartCount,setCartCount] = useState(0);
+  const [cartCount,setCartCount] = useState(0);     // no. of items in cart
   const [cart,setCart] = useState(null);
 
   const fetchCartCount = async () => {
@@ -48,17 +57,18 @@ function App() {
 
   useEffect(() => {
     fetchCartCount();
-  }, []);
+  }, [cart]);
 
-  
+  // if cart not present loading...
   if (!cart) {
   return (
     <div className="flex justify-center mt-10">
       <OrbitProgress variant="dotted" dense color="#6de9a4" size="medium" text="" textColor="" />
     </div>
   );
-}
+  }
 
+  // Total Price = (price*quentity) of all cart items
   const total = (cart.items.reduce(
     (sum, item) => sum + item.product.price * item.quantity,
     0
@@ -69,8 +79,7 @@ function App() {
     <>
       <div className="bg-linear-to-br from-teal-50 via-cyan-50 to-emerald-50">
 
-
-        <Navbar cartCount={ cartCount } />
+        <Navbar cartCount = { cartCount } />
 
         <main className="p-6">
           <Routes>
@@ -79,6 +88,7 @@ function App() {
             <Route path="/" element={<ProductList refreshCart={fetchCartCount} />} />
             <Route path="/product/:id" element={<ProductDetail refreshCart={fetchCartCount} />} />
             
+            {/* Private Routes*/}
             <Route path='/profile' element={ <PrivateRoute> <Profile /> </PrivateRoute> } />
             <Route path="/cart" element={ <PrivateRoute> <Cart refreshCart={fetchCartCount} cart={cart} total={total}/> </PrivateRoute> } />
             <Route path="/checkout" element={ <PrivateRoute> <Checkout total={total} /> </PrivateRoute> } />
